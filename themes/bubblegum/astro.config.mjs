50d9f3125f7d63047a0fcf6aaba9cb2d82f7bfb2
Pelle van der Knaap <pelle.van.der.knaap@oberon.nl>
2022-12-26 11:24:30 +0100

    customize theme

--- themes/bubblegum/astro.config.mjs
+++ themes/bubblegum/astro.config.mjs
@@ -3,21 +3,22 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-/* 
+/*
   We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
   In local development, your SEO meta tags will have localhost URL.
   In built production websites, your SEO meta tags should have your website URL.
-  So we give our website URL here and the template will know what URL to use 
+  So we give our website URL here and the template will know what URL to use
   for meta tags during build.
   If you don't know your website URL yet, don't worry about this
   and leave it empty or use localhost URL. It won't break anything.
 */
-
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 const SERVER_PORT = 9003;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://blogster-bubblegum.netlify.app";
+const LIVE_URL = "https://blog.betrouw.nl";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -27,13 +28,17 @@ if (isBuild) {
   BASE_URL = LIVE_URL;
 }
 
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
+  server: {
+    port: SERVER_PORT
+  },
   site: BASE_URL,
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), prefetch(), partytown()]
 });
