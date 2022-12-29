d623e1058c9696015eb86cf2e29ee9542e0c7a8d
Pelle van der Knaap <pelle.van.der.knaap@oberon.nl>
2022-12-29 17:57:36 +0100

    update font article

--- themes/bubblegum/content/blog/font-optimization.md
+++ themes/bubblegum/content/blog/font-optimization.md
@@ -6,6 +6,44 @@ external: false
 ---
 
 # Font Subsetting
+Font subsetting is a practice commonly used in web development to optimize the performance of a website. 
+When a webpage is loaded, all the fonts that are used on the page are downloaded by the user. 
+This significantly increases the page load time if the font files aren't optimized. 
+Font subsetting is a technique that decreases the size of the font files, and therefore speeding up the page loads.
+It involves only including the characters that are actually used on the page, rather than the entire font. 
+In most cases, this significantly reduces the file size of the font. 
+However, it's important to note that font subsetting can have an impact on the appearance of the text.
+If characters are removed from the font, but are still used on the page, the browser will substitute them with a fallback.
+This can break the design of the page, and should be avoided if possible.
+
+To summarize, font subsetting is extremely worthwhile, but should be done carefully to prevent weird charachter glitches.
+
+## The unicode-range property
+Thesedays, the most performant and easiest method to subset your fonts is to use the `unicode-range` property.
+This property allows you to specify which characters you want to include in the font file.
+
+Since the most common usecase is to use a range of unicodes (charachters) from a font, you can specify a range.
+You can find the unicode ranges for each font on [Unicode Table](https://unicode-table.com/en/#control-character).
+For Latin, you want to use ```U+0020-007F```, which includes all the characters from space to tilde.
+
+## Fun Fact
+You can use the `unicode-range` property to include multiple ranges of characters from different fonts.
+This allows you to mix fonts, and for example use a serif font for certain characters and a mono font for other characters.
+Most likely, you won't need this, but it's fun to experiment with and might be useful someday.
+
+## Google fonts subset API
+If you use Google Fonts to fetch your fonts, it is incredibly easy to subset your fonts.
+You can use the previously discussed CSS property in your font-face, or you can use the Google Fonts subset API.
+Using the Gooel Fonts API is a better method if you want to refrain from manually specifying font-face rules.
+Simply add the text `&text=` to the end of the URL, and then add the characters that you want to use.
+Alternatively, you can select a few presets of characters that are commonly used. You can add `&subset=latin` or `&subset=latin-ext` to the URL for example.
+
+
+// https://unicode-table.com/en/#control-character
+// https://www.w3.org/TR/PFE-evaluation/#static
+
+
+
 
 {% font /%}
 
--- themes/bubblegum/src/components/FontWidget/FontWidget.svelte
+++ themes/bubblegum/src/components/FontWidget/FontWidget.svelte
@@ -1,186 +1,193 @@
 <script>
-  import clonedeep from "lodash.clonedeep";
-  import uniq from "lodash.uniq";
-  import { onMount } from "svelte";
+    import clonedeep from "lodash.clonedeep";
+    import uniq from "lodash.uniq";
+    import {onMount} from "svelte";
+    import RangeSlider from "svelte-range-slider-pips";
 
-  const properties = {
-    "font-size": {
-      min: 8,
-      max: 50,
-      value: 16,
-      step: 1,
-      extension: "px",
-      label: "Font Size",
-    },
-    "line-height": {
-      min: 0,
-      max: 5,
-      value: 1,
-      step: 0.05,
-      extension: "",
-      label: "Line Height",
-    },
-    "font-weight": {
-      min: 100,
-      max: 900,
-      value: 300,
-      step: 100,
-      extension: "",
-      label: "Font Weight",
-    },
-    "letter-spacing": {
-      min: -2,
-      max: 2,
-      value: 0,
-      step: 0.05,
-      extension: "px",
-      label: "Letter Spacing",
-    },
-    "word-spacing": {
-      min: -2,
-      max: 2,
-      value: 0,
-      step: 0.05,
-      extension: "px",
-      label: "Word Spacing",
-    },
-  };
+    const properties = {
+        "font-size": {
+            min: 8,
+            max: 50,
+            value: 16,
+            step: 1,
+            extension: "px",
+            label: "Font Size",
+        },
+        "line-height": {
+            min: 0,
+            max: 5,
+            value: 1,
+            step: 0.05,
+            extension: "",
+            label: "Line Height",
+        },
+        "font-weight": {
+            min: 100,
+            max: 900,
+            value: 300,
+            step: 100,
+            extension: "",
+            label: "Font Weight",
+        },
+        "letter-spacing": {
+            min: -2,
+            max: 2,
+            value: 0,
+            step: 0.05,
+            extension: "px",
+            label: "Letter Spacing",
+        },
+        "word-spacing": {
+            min: -2,
+            max: 2,
+            value: 0,
+            step: 0.05,
+            extension: "px",
+            label: "Word Spacing",
+        },
+    };
 
-  const fonts = {
-    fallback: properties,
-    webFont: clonedeep(properties),
-  };
+    const fonts = {
+        fallback: properties,
+        webFont: clonedeep(properties),
+    };
 
-  $: stylingFallback = Object.entries(fonts.fallback)
-    .map(([key, value]) => {
-      return [`${key}: ${value.value}${value.extension}`];
-    })
-    .join("; ");
+    $: stylingFallback = Object.entries(fonts.fallback)
+        .map(([key, value]) => {
+            return [`${key}: ${value.value}${value.extension}`];
+        })
+        .join("; ");
 
-  $: stylingWebFont = Object.entries(fonts.webFont)
-    .map(([key, value]) => {
-      return [`${key}: ${value.value}${value.extension}`];
-    })
-    .join("; ");
+    $: stylingWebFont = Object.entries(fonts.webFont)
+        .map(([key, value]) => {
+            return [`${key}: ${value.value}${value.extension}`];
+        })
+        .join("; ");
 
-	let fallbackFont = "sans-serif"
-  let webFontName = "Roboto";
+    let fallbackFont = "sans-serif"
+    let webFontName = "Roboto";
 
-  const fetchWebFont = () => {
-    var url = (url =
-      "https://fonts.googleapis.com/css?family=" +
-      webFontName.trim() +
-      ":300,300i,400,400i,700,700i,900,900i");
+    const fetchWebFont = () => {
+        var url = (url =
+            "https://fonts.googleapis.com/css?family=" +
+            webFontName.trim() +
+            ":300,300i,400,400i,700,700i,900,900i");
 
-    var link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = url;
-    document.head.appendChild(link);
-  };
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = url;
+        document.head.appendChild(link);
+    };
 
-  onMount(async () => {
-    fetchWebFont()
+    onMount(async () => {
+        fetchWebFont()
 
-    // &text=abc is already supported, subsets not yet
-    const res = await fetch("https://fonts.googleapis.com/css?family=Roboto+Mono&text=abc", { method: "GET" })
-    const text = await res.text()
-    console.log(text)
-    const re = /(?<=url\()(.*)(?=\) format)/g;
-    const matches = [...text.matchAll(re)].map((match) => match[0]);
-    console.log(uniq(matches))
-    let length = 0;
-    await Promise.all(uniq(matches).map(async link => {
-          const res = await fetch(link, { method: "HEAD" })
-          length += parseInt(res.headers.get("content-length")) / 1000
-    }))
-    console.log(Math.floor(length) + "kb")
+        // &text=abc is already supported, subsets not yet
+        const res = await fetch("https://fonts.googleapis.com/css?family=Roboto+Mono&text=abc", {method: "GET"})
+        const text = await res.text()
+        console.log(text)
+        const re = /(?<=url\()(.*)(?=\) format)/g;
+        const matches = [...text.matchAll(re)].map((match) => match[0]);
+        console.log(uniq(matches))
+        let length = 0;
+        await Promise.all(uniq(matches).map(async link => {
+            const res = await fetch(link, {method: "HEAD"})
+            length += parseInt(res.headers.get("content-length")) / 1000
+        }))
+        console.log(Math.floor(length) + "kb")
+    });
 
-  });
+    let RangeSliderComponent;
+    onMount(() => {
+        RangeSliderComponent = RangeSlider;
+    });
+
+    let test = [50]
+
 </script>
 
 <div class="grid grid-cols-2">
-  <div>
-	    <div>
-      <input type="text" bind:value={fallbackFont} class="bg-bg-body w-32 border-text-link border-b-2"/>
-    </div>
-
-    {#each Object.entries(fonts.fallback) as [key, value]}
-      <div>
-        <p class="text-white !my-0">{value.label}</p>
-
-        <label>
-          <input
-            type="range"
-            bind:value={value.value}
-            min={value.min}
-            max={value.max}
-            step={value.step}
-          />
-
-          <p>{value.value}{value.extension}</p>
-        </label>
-      </div>
-    {/each}
-  </div>
-
-  <div>
     <div>
-      <input type="text" bind:value={webFontName} class="bg-bg-body w-32 border-text-link border-b-2"/>
-      <button on:click={fetchWebFont}>Fetch Font</button>
+        <div>
+            <input type="text" bind:value={fallbackFont} class="bg-bg-body w-32 border-text-link border-b-2"/>
+        </div>
+
+        {#each Object.entries(fonts.fallback) as [key, value]}
+            <div>
+                <p class="text-white !my-0">{value.label}</p>
+
+                <div>
+                    <svelte:component this={RangeSliderComponent} values={test} pips min={value.min}
+                                      max={value.max}
+                                      step={value.step} class="w-full"/>
+
+                    <input bind:value={test[0]} />
+
+                    <p>{value.value}{value.extension}</p>
+                </div>
+            </div>
+        {/each}
     </div>
 
-    {#each Object.entries(fonts.webFont) as [key, value]}
-      <div>
-        <p class="text-white !my-0">{value.label}</p>
+    <div>
+        <div>
+            <input type="text" bind:value={webFontName} class="bg-bg-body w-32 border-text-link border-b-2"/>
+            <button on:click={fetchWebFont}>Fetch Font</button>
+        </div>
 
-        <label>
-          <input
-            type="range"
-            bind:value={value.value}
-            min={value.min}
-            max={value.max}
-            step={value.step}
-          />
+        {#each Object.entries(fonts.webFont) as [key, value]}
+            <div>
+                <p class="text-white !my-0">{value.label}</p>
 
-          <p>{value.value}{value.extension}</p>
-        </label>
-      </div>
-    {/each}
-  </div>
+                <label>
+                    <input
+                            type="range"
+                            bind:value={value.value}
+                            min={value.min}
+                            max={value.max}
+                            step={value.step}
+                    />
+
+                    <p>{value.value}{value.extension}</p>
+                </label>
+            </div>
+        {/each}
+    </div>
 </div>
 
 <div class="relative">
-  <p style={stylingFallback} style:font-family={fallbackFont} class="m-0">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
+    <p style={stylingFallback} style:font-family={fallbackFont} class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
 
-  <p
-    style={stylingWebFont}
-    style:font-family={webFontName}
-    class="absolute top-0 left-0 text-primary-blue !my-0"
-  >
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
+    <p
+            style={stylingWebFont}
+            style:font-family={webFontName}
+            class="absolute top-0 left-0 text-primary-blue !my-0"
+    >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
 </div>
 
 Based on <a href="https://meowni.ca/font-style-matcher/" target="_about" ref="noopener">Monica Dinculescu</a> her work
 
 <style>
-  label {
-    display: flex;
-  }
-  input,
-  p {
-    margin: 6px;
-  }
+    label {
+        display: flex;
+    }
+
+    input,
+    p {
+        margin: 6px;
+    }
 </style>
